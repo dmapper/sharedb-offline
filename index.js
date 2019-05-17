fa96989ce9985bb42353546a343b7c96d0c03a09
Artur Zayats <zag2art@gmail.com>
2019-05-17 11:42:23 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const MessageSocket = require('./lib/MessageSocket')
+const MessageStream = require('./lib/MessageStream')
+const Messenger = require('./lib/Messenger')
+const RPC = require('./lib/RPC')
+const Socket = require('./lib/Socket')
+
+module.exports = {
+  MessageSocket,
+  MessageStream,
+  Messenger,
+  RPC,
+  Socket
+}
