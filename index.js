2a2f6a4b47bf7c3ba83358071a7efea12b7e5f35
Artur Zayats <zag2art@gmail.com>
2019-05-17 12:30:26 +0300

    add workerRpc

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const MessageStream = require('./lib/MessageStream')
 const Messenger = require('./lib/Messenger')
 const RPC = require('./lib/RPC')
 const Socket = require('./lib/Socket')
+const workerRpc = require('./lib/workerRpc')
 
 module.exports = {
   MessageSocket,
   MessageStream,
   Messenger,
   RPC,
-  Socket
+  Socket,
+  workerRpc
 }
